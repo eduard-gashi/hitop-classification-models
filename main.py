cf543c40668fd80d22d1f05edc932b042a1cfaba
Eduard Gashi <gashi.eduard@gmx.de>
2025-10-31 08:41:49 +0100

    Map Codes to questions by creating multiindex

--- main.py
+++ main.py
@@ -13,17 +13,49 @@ def load_data():
     return df_test_variables, df_pre_therapy_ratings, df_post_therapy_ratings
 
 
-def main():
-    df_test_variables, df_pre_therapy_ratings, df_post_therapy_ratings = load_data()
-    print("Test Variables DataFrame:")
-    print(df_test_variables.head())
+def attach_metadata_as_multiindex(therapy_ratings_df, metadata_df, metadata_column='Variablenlabel'):
+    # Delete duplicates in metadata and therapy_ratings DataFrames
+    if metadata_df['Variablenname'].duplicated().any():
+        metadata_df = metadata_df.drop_duplicates(subset=['Variablenname'], keep='first')
+        
+    if therapy_ratings_df.columns.duplicated().any():
+        therapy_ratings_df = therapy_ratings_df.loc[:, ~therapy_ratings_df.columns.duplicated(keep='first')]
     
-    print("\nPre Therapy Ratings DataFrame:")
-    print(df_pre_therapy_ratings.head())
+    # Lookupup-Table: Mapping from Codes to desired metadata 
+    metadata_lookup = metadata_df.set_index('Variablenname')[metadata_column]
+    sorted_metadata = metadata_lookup.reindex(therapy_ratings_df.columns)
+        
+    # Create MultiIndex from metadata
+    multi_index = pd.MultiIndex.from_arrays(
+        [
+            sorted_metadata.fillna(f'Unbekannt: {metadata_column}'),  # Layer 1: Metadata labels
+            sorted_metadata.index # Layer 2: Original variable codes
+        ],
+        names=[metadata_column, 'Code']
+    )
     
-    print("\nPost Therapy Ratings DataFrame:")
-    print(df_post_therapy_ratings.head())
+    therapy_ratings_df.columns = multi_index
+    
+    return therapy_ratings_df
 
+
+def main():
+    df_metadata, df_pre_therapy_ratings, df_post_therapy_ratings = load_data()
+        
+        
+    df_pre_therapy_ratings = attach_metadata_as_multiindex(
+        therapy_ratings_df=df_pre_therapy_ratings.copy(),
+        metadata_df=df_metadata,
+        metadata_column='Variablenlabel' # Possible columns [Frage, Variablenlabel, Test, Skala, Anmerkung]
+    )
+    
+    df_post_therapy_ratings = attach_metadata_as_multiindex(
+        therapy_ratings_df=df_post_therapy_ratings.copy(),
+        metadata_df=df_metadata,
+        metadata_column='Variablenlabel' # Possible columns [Frage, Variablenlabel, Test, Skala, Anmerkung]
+    )
+    print("Pre-Therapy Ratings with MultiIndex:")
+    
 
 if __name__ == "__main__":
     main()
