4b98311cf4addca1c35cdbfb30c92df0025f54c8
Maxim Valuta <valutamaxim@gmail.com>
2025-11-16 15:13:58 +0100

    Refactor data_sampler.py

--- src/processing/data_sampler.py
+++ src/processing/data_sampler.py
@@ -1,19 +1,11 @@
 import pandas as pd
-import os 
+from src.processing.data_loader import load_data
+from config import(
+    SAMPLED_PRE_DATASET,
+    SAMPLED_POST_DATASET,
+)
 
-base_path = os.getcwd()
-
-original_dataset_path = os.path.join(base_path, "data", "raw")
-
-pre_dataset_path = os.path.join(original_dataset_path, "pre_dataset_standardized.xlsx")
-post_dataset_path = os.path.join(original_dataset_path, "post_dataset.xlsx")
-
-sampled_dataset_path = os.path.abspath(os.path.join(original_dataset_path, "..", "processed\\"))
-
-# Einlesen der Original-Datensätze
-df_pre = pd.read_excel(pre_dataset_path)
-df_post = pd.read_excel(post_dataset_path)
-
+df_test_vars, df_pre, df_post = load_data()
 
 pre_sampled = df_pre.sample(n=1000, random_state=42)
 ids = pre_sampled["Code"]
@@ -22,5 +14,5 @@ ids = pre_sampled["Code"]
 post_sampled = df_post[df_post["Code"].isin(ids)]
 
 # Exportieren der gesampelten Datensätze
-pre_sampled.to_excel(os.path.join(sampled_dataset_path, "pre_dataset.xlsx"), index=False)
-post_sampled.to_excel(os.path.join(sampled_dataset_path, "post_dataset.xlsx"), index=False)
+pre_sampled.to_excel(SAMPLED_PRE_DATASET, index=False)
+post_sampled.to_excel(SAMPLED_POST_DATASET, index=False)
