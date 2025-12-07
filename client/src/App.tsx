fdcdc2262587ea25bbd171ecdfdf30efae9819ec
Eduard Gashi <gashi.eduard@gmx.de>
2025-12-07 11:26:51 +0100

    Create Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
 
+import Home from './pages/Home';
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <main style={{ display: "flex", flexDirection: "column", minHeight: "90vh" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/diagnosen" element={<div>About Page</div>} />
+          <Route path="/frageboegen" element={<div>Fragebogen Explorer</div>} />
+          <Route path="/patienten" element={<div>Fragebogen Explorer</div>} />
+          <Route path="/faq" element={<div>Fragebogen Explorer</div>} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
 
