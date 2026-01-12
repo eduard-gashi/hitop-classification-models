3f8b4e2e2f6d9d26b0eab746a2005d9f7f805b47
Eduard Gashi <gashi.eduard@gmx.de>
2026-01-12 20:32:10 +0100

    Update requirements and allow dns of virtual machine

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'ki-psychologie.student.vm.pve.fa1.hs-furtwangen.de',
+      '141.28.73.25',
+      'localhost'
+    ]
+  }
 })
